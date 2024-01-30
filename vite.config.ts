86fdabc199166996bb37a2cdcec0f0819627d39a
Eduarddo Drozda <edu_drozda@hotmail.com>
2024-01-30 00:07:36 -0300

    exclude folders to coverage

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,17 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsConfigPaths()],
+  test: {
+    include: ['src/**/*.spec.ts'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      './src/config/**',
+    ],
+    coverage: {
+      exclude: ['./src/shared/mocks/**', 'node_modules'],
+    },
+  },
 })
